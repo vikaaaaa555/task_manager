db6428dfbb627f72298388e0ecdffaefc32be7ea
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-06-12 16:21:22 +0300

    feat: Add documentation

--- lib/presentation/screens/sign_up/sign_up_screen_wrapper.dart
+++ lib/presentation/screens/sign_up/sign_up_screen_wrapper.dart
@@ -6,6 +6,13 @@ import '../../bloc/sign_up/sign_up_bloc.dart';
 import '../home/home_screen_wrapper.dart';
 import 'sign_up_screen.dart';
 
+/// This widget listens to the [SignUpBloc] state
+///
+/// It manages the following states:
+/// - [SignUpInitial]: Shows the sign-up form
+/// - [SignUpLoading]: Shows a loading indicator
+/// - [SignUpSuccess]: Navigates to home screen
+/// - [SignUpFailure]: Displays error message
 class SignUpScreenWrapper extends StatelessWidget {
   const SignUpScreenWrapper({super.key});
 
@@ -13,23 +20,30 @@ class SignUpScreenWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<SignUpBloc, SignUpState>(
       builder: (context, state) {
-        if (state is SignUpInitial) {
-          return SignUpScreen();
-        } else if (state is SignUpLoading) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (state is SignUpSuccess) {
-          return HomeScreenWrapper();
-        } else if (state is SignUpFailure) {
-          return Center(
-            child: Text(S.of(context).errorCodeAndMessage(
-              state.code as Object,
-              state.message as Object,
-            )),
-          );
-        } else {
-          return Center(child: Text(S.of(context).unexpectedError));
+        switch (state) {
+          case SignUpInitial():
+            return _buildInitialState();
+          case SignUpLoading():
+            return _buildLoadingState();
+          case SignUpSuccess():
+            return _buildSuccessState();
+          case SignUpFailure():
+            return _buildFailureState(context, state);
         }
       },
     );
   }
+
+  Widget _buildInitialState() => const SignUpScreen();
+
+  Widget _buildLoadingState() =>
+      const Center(child: CircularProgressIndicator());
+
+  Widget _buildSuccessState() => const HomeScreenWrapper();
+
+  Widget _buildFailureState(context, state) => Center(
+    child: Text(S.of(context).errorCodeAndMessage(
+        state.code as Object,
+        state.message as Object)),
+  );
 }
