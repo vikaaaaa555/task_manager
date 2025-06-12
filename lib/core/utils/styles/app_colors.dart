6d0df63dfe714f41fbe1bed2b011a8b4985a2017
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-06-12 16:05:53 +0300

    feat: Create themes

---
+++ lib/core/utils/styles/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color blackCherry = Color(0xFF2b0f26);
+  static const Color blackSabbath = Color(0xFF250320);
+  static const Color purpleExcellency = Color(0xFF9C388C);
+  static const Color placebo = Color(0xFFE8E4E7);
+  static const Color violetVogue = Color(0xFFE7E2E6);
+  static const Color purplePoodle = Color(0xFFDBB0D1);
+}
