4ce40fdc5750f88e899270bd55330c56a1702f12
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-06-12 15:50:51 +0300

    feat: Delegate localization

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:task_manager/presentation/bloc/sign_up/sign_up_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'core/options/localization/l10n.dart';
 import 'core/services/app_dependencies.dart';
 import 'core/utils/styles/theme_config.dart';
 import 'features/auth/domain/use_cases/create_account_with_email_and_password.dart';
+import 'presentation/bloc/sign_up/sign_up_bloc.dart';
+import 'presentation/screens/sign_in/sign_in_screen_wrapper.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,15 +18,24 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<SignUpBloc>(
-          create: (context) =>
-              SignUpBloc(createAccountWithEmailAndPasswordUseCase:
-              sl<CreateAccountWithEmailAndPasswordUseCase>()),
+          create:
+              (context) => SignUpBloc(
+                createAccountWithEmailAndPasswordUseCase:
+                    sl<CreateAccountWithEmailAndPasswordUseCase>(),
+              ),
         ),
       ],
       child: MaterialApp(
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          home:
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        home: SignInScreenWrapper(),
       ),
     );
   }
