c77fd4492955b3889148890ccd016df6159ee51c
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-06-12 21:37:01 +0300

    feat: Register dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'core/options/firebase_options.dart';
+import 'core/services/app_dependencies.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await registerAppDependencies();
   runApp(const MyApp());
 }
