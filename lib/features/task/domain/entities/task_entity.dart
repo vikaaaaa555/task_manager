281a0b9af3e934aaf572e4c1207e15d474210688
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-06-13 17:08:18 +0300

    feat: Add id param

--- lib/features/task/domain/entities/task_entity.dart
+++ lib/features/task/domain/entities/task_entity.dart
@@ -1,9 +1,11 @@
 class TaskEntity {
+  final int id;
   final String title;
   final String? description;
   final DateTime dueDate;
 
   const TaskEntity({
+    required this.id,
     required this.title,
     this.description,
     required this.dueDate,
