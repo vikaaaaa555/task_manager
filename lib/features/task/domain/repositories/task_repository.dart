426603dd9f9ad2052ce5f8056acd42aa830d1767
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-06-13 17:08:49 +0300

    feat: Create a task abstract repository

---
+++ lib/features/task/domain/repositories/task_repository.dart
@@ -0,0 +1,16 @@
+import '../entities/task_entity.dart';
+
+abstract class TaskRepository {
+  Future<List<TaskEntity>> getAllTasks();
+
+  Future<void> createTask(String title, String? description, DateTime dueDate);
+
+  Future<void> updateTask(
+    int id,
+    String? title,
+    String? description,
+    DateTime? dueDate,
+  );
+
+  Future<void> deleteTask(int id);
+}
